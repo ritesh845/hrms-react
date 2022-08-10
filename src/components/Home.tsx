f1a99d7d393bd16b1e43edb0a5dc71342725b4d1
Ritesh Panchal <ritesh.panchal@esparkbizmail.com>
2022-08-10 16:14:00 +0530

    react

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,36 @@
-import { Typography } from '@mui/material';
-import * as React from 'react';
+import { Typography,Card,Box } from '@mui/material';
+import * as React  from 'react';
 
 const Home:React.FC = () => {
+    
+    
     return (
-        <>     
-          <Typography>
-              Home
+        <>  
+            <Box>
+                <Typography variant="h5" sx={{ mb: 5 }}>
+                    Hi, Welcome back
+                </Typography>
+
+                <Card sx={{
+                        py: 5,
+                        boxShadow: 0,
+                        minWidth:275,
+                        textAlign: 'center',
+                        bgColor:"red"
+                    }}
+                    
+                >
+                    <Typography variant="h3">100</Typography>
+
+                    <Typography variant="subtitle2" sx={{ opacity: 0.72 }}>
+                        Users
+                    </Typography>
+
+                </Card>    
+                
+            </Box>  
+           
             
-          </Typography>     
         </>
     );
 }
--- src/pages/user/User.tsx
+++ src/pages/user/User.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect,useState } from 'react'
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
-import { Toolbar } from '@mui/material';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { Button, Typography,Stack,Modal,Fade,Box, Grid,TextField } from '@mui/material';
 import UserService from '../../services/user.services';
 import { UserModel } from '../../models/user';
+import Backdrop from '@mui/material/Backdrop';
+
 const columns: GridColDef[] = [
   
   { field: 'id', headerName: 'ID', width: 70 },
@@ -15,10 +17,21 @@ const columns: GridColDef[] = [
   }
 ];
 
-
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '25%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 800,
+  bgcolor: 'background.paper',
+  border: '1px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 const User:React.FC = () => {
   const [users, setUsers] = useState<UserModel[]>([]);
+  const [open, setOpen] = useState(false);
 
   const getUsers = async () => {
       await UserService.getUser().then((res:any) => {
@@ -34,14 +47,64 @@ const User:React.FC = () => {
       getUsers();
   }, []);
 
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+
   return (
     
     <div style={{ height: 400, width: '100%' }}>
-      <Toolbar>
-          
-      </Toolbar>
+      <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+          <Typography variant="h6" gutterBottom>
+            User List
+          </Typography>
+          <Button variant="contained" onClick={handleOpen}>
+            New User
+          </Button>
+        </Stack>
+        <Modal
+          aria-labelledby="spring-modal-title"
+          aria-describedby="spring-modal-description"
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500,
+          }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+              <Typography id="spring-modal-title" variant="h6" component="h2">
+                Add New User
+              </Typography>
+              <Box
+                  component="form"
+                  sx={{
+                    '& .MuiTextField-root': { m: 1, width: '25ch' },
+                  }}
+                  noValidate
+                  autoComplete="off"
+                >
+                <TextField
+                    id="outline-full-name-input"
+                    label="Full Name"
+                    rows={4}
+                  />
+                  <TextField
+                    id="outline-email-input"
+                    label="Email"
+                    className='ml-[10px]'
+                  />
+                </Box>
+          </Box>
+        </Fade>
+      </Modal>
+
+
+
       {
-        users.length !=0 ? 
+        users.length !== 0 ? 
           <DataGrid
             rows={users}
             columns={columns}
